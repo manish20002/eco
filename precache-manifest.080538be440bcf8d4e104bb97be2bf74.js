630e22e0b1d6e412fc400a86ae9d43abacd3e891
Manish Thirani <73772706+manish20002@users.noreply.github.com>
2024-04-01 06:28:58 +0000

    Updates

---
+++ precache-manifest.080538be440bcf8d4e104bb97be2bf74.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec1ef0bc86f92c8a4992",
+    "url": "/eco/static/css/main.5e1c3590.chunk.css"
+  },
+  {
+    "revision": "ec1ef0bc86f92c8a4992",
+    "url": "/eco/static/js/main.ec1ef0bc.chunk.js"
+  },
+  {
+    "revision": "692a8372bce424561495",
+    "url": "/eco/static/js/1.692a8372.chunk.js"
+  },
+  {
+    "revision": "f4aee2c9064ce22d816a",
+    "url": "/eco/static/css/2.cb54c858.chunk.css"
+  },
+  {
+    "revision": "f4aee2c9064ce22d816a",
+    "url": "/eco/static/js/2.f4aee2c9.chunk.js"
+  },
+  {
+    "revision": "42a229c744e29ee781ae",
+    "url": "/eco/static/js/runtime~main.42a229c7.js"
+  },
+  {
+    "revision": "1d258e3276a7ef44770d122b456d4cff",
+    "url": "/eco/static/media/plates.1d258e32.jpg"
+  },
+  {
+    "revision": "a26298926a6abc88a77811aedf25fe83",
+    "url": "/eco/static/media/bowls.a2629892.jpg"
+  },
+  {
+    "revision": "8389dab9ed83b5f079be85976712c010",
+    "url": "/eco/static/media/cups1.8389dab9.jpg"
+  },
+  {
+    "revision": "dd56db1328415355883a2aa5ceb7a228",
+    "url": "/eco/static/media/spoons1.dd56db13.jpg"
+  },
+  {
+    "revision": "6b0f4ff624e4815f3d7faabce28464a4",
+    "url": "/eco/static/media/logoefe.6b0f4ff6.png"
+  },
+  {
+    "revision": "8ca4acf35c2e6ef1908e85d146026938",
+    "url": "/eco/static/media/bgrem.8ca4acf3.png"
+  },
+  {
+    "revision": "a38dabefa8bf67d58158585d639b089a",
+    "url": "/eco/static/media/eco-p3.a38dabef.jpg"
+  },
+  {
+    "revision": "d5f23cdb113570253bf777a2c3a946cd",
+    "url": "/eco/index.html"
+  }
+];
